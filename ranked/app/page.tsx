f0d0885f7e76b021824384d3e08ebe288621d512
Tanish <tkhadse2@illinois.edu>
2023-05-24 19:18:37 -0500

    layout tsx + file structure

--- ranked/app/page.tsx
+++ ranked/app/page.tsx
@@ -3,12 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/rankedlogo.png"
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
+
     </main>
   )
 }
